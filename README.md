61435fa33e2a31d4bcc6bd20769769485c06f57c
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-06 22:48:38 +0200

    Support for TYPO3 12

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-base_html
-=========
-This provides the htmlpurifier library.
-It is required for older versions of base_excel.
+# TYPO3 extension base_html
+
+## What is does
+
+This extension brings the "HTML Purifier" PHP classes "ezyang/htmlpurifier" to TYPO3.
+
+## What is the origin
+
+See http://htmlpurifier.org/download
+
+## Documentation
+
+http://htmlpurifier.org/docs
 
--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'HTML Filter',
     'description' => 'Standards compliant HTML filter written in PHP (htmlpurifier)',
     'category' => 'misc',
-    'version' => '0.0.1',
+    'version' => '4.15.0',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Edward Z. Yang, Franz Holzinger',
@@ -16,8 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.1.99',
-            'typo3' => '9.5.0-11.5.99'
+            'typo3' => '10.4.0-12.4.99'
         ],
         'conflicts' => [
         ],
